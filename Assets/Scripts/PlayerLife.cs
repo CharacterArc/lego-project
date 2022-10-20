ba9358b86098a67aa95af30078a15d07dbe7a32f
Soumeii <113452242+Soumeii@users.noreply.github.com>
2022-10-19 21:47:52 -0500

    Start and Side Scroller Scenes

---
+++ Assets/Scripts/PlayerLife.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerLife : MonoBehaviour
+{
+    private Animator anim;
+    private Rigidbody2D rb;
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Trap"))
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        anim.SetTrigger("death");
+        rb.bodyType = RigidbodyType2D.Static;
+    }
+
+    private void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
---
+++ Assets/Scripts/StartMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour
+{
+    [SerializeField] AudioSource buttonSound;
+
+    public void StartCraft()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        buttonSound.Play();
+    }
+    public void StartInstruct()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+        buttonSound.Play();
+    }
+}
