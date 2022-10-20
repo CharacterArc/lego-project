d2699e1af1740a9d69a08b83c5e24775a0990020
Nori <112436078+n-goodrum@users.noreply.github.com>
2022-10-20 10:15:52 -0500

    Main Menu Start Button
    Main Menu functionality.

---
+++ Assets/Scripts/GameStart.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameStart : MonoBehaviour
+{
+    public void Building()
+    {
+        SceneManager.LoadScene("Test1");
+    }
+}
