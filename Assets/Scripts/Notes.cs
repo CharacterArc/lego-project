cf5354bc5cbe96a3ad53543a996f2eaf5bcd3e21
Soumeii <113452242+Soumeii@users.noreply.github.com>
2022-10-25 13:47:23 -0500

    Attempt to add notes (1)

---
+++ Assets/Scripts/Notes.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Notes : MonoBehaviour
+{
+    public string message;
+    private bool showGui = false;
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player") showGui = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player") showGui = false;
+
+    }
+
+    void OnGUI()
+    {
+        if (showGui) GUI.Box(new Rect(0, 0, 1080, 100), message);
+    }
+}
