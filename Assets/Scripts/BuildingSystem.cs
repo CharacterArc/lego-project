b680f6124915da46da260f63687a392ba5a670bc
Mitchell77777 <mitchell.l.chesne@gmail.com>
2022-10-27 03:34:30 -0500

    UI UPDATE PRESS I

--- Assets/Scripts/BuildingSystem.cs
+++ Assets/Scripts/BuildingSystem.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class BuildingSystem : MonoBehaviour
 {
-
     [SerializeField]
     private Camera playerCamera;
 
---
+++ Assets/Scripts/UI.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI : MonoBehaviour
+{
+    public GameObject inventory;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        inventory.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("i"))
+        {
+            if (inventory.activeSelf == false)
+            {
+                inventory.SetActive(true);
+            }
+            else
+            {
+                inventory.SetActive(false);
+            }
+        }
+    }
+}
