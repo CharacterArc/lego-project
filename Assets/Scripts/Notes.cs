8e811e9f547a7ba2468faf046c9192ccda4d319e
Soumeii <113452242+Soumeii@users.noreply.github.com>
2022-11-08 10:53:31 -0600

    text fix kinda

--- Assets/Scripts/Notes.cs
+++ Assets/Scripts/Notes.cs
@@ -7,18 +7,19 @@ public class Notes : MonoBehaviour
 {
     [SerializeField] private Text txt;
     [SerializeField] private Image back;
-    [SerializeField] private Image levelback;
+    [SerializeField] private Image extra;
     private void OnTriggerEnter2D(Collider2D col)
     {
         if (col.gameObject.tag == "Player") back.enabled = true;
         if (col.gameObject.tag == "Player") txt.enabled = true;
-        if (col.gameObject.tag == "Player") levelback.enabled = true;
+        if (col.gameObject.tag == "Player") extra.enabled = true;
     }
 
     private void OnTriggerExit2D(Collider2D col)
     {
         if (col.gameObject.tag == "Player") txt.enabled = false;
         if (col.gameObject.tag == "Player") back.enabled = false;
+        if (col.gameObject.tag == "Player") extra.enabled = false;
     }
 
 }
