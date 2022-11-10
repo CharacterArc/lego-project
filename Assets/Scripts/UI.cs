ac2aa07e8bc3aff3612a7a91f07434150059d0c5
Mitchell77777 <mitchell.l.chesne@gmail.com>
2022-11-10 11:06:02 -0600

    added chat app into the test scene, press t

--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -6,17 +6,30 @@ using UnityEngine.UI;
 public class UI : MonoBehaviour
 {
     public GameObject inventory;
+    public GameObject chat;
 
 
     // Start is called before the first frame update
     void Start()
     {
         inventory.SetActive(true);
+        chat.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown("t"))
+        {
+            if (chat.activeSelf == false)
+            {
+                chat.SetActive(true);
+            }
+            else
+            {
+                chat.SetActive(false);
+            }
+        }
         if (Input.GetKeyDown("i"))
         {
             if (inventory.activeSelf == false)
